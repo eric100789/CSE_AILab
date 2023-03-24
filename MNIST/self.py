8dfdf8630af294defef167117bc69bdecd9e58f7
eric100789 <eric100147@gmail.com>
2023-03-25 01:32:22 +0800

    mmist

---
+++ MNIST/self.py
@@ -0,0 +1,32 @@
+import torch
+from torch.utils import data as data_
+import torch.nn as nn
+from torch.autograd import Variable
+import matplotlib.pyplot as plt
+import torchvision
+
+EPOCH = 1
+BATCH_SIZE = 50
+LR = 0.001
+DOWNLOAD_MNIST = False
+
+train_data = torchvision.datasets.MNIST(root='./mnist', train=True, transform=torchvision.transforms.ToTensor(),
+                                        download=DOWNLOAD_MNIST)
+
+train_loader = data_.DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=True, num_workers=2)
+test_data = torchvision.datasets.MNIST(root='./mnist/', train=False)
+test_x = torch.unsqueeze(test_data.test_data, dim=1).type(torch.FloatTensor)[:2000] / 255.
+test_y = test_data.test_labels[:2000]
+
+
+class ConvNet(nn.Module):
+
+    def __init__(self):
+        super(ConvNet, self).__init__()
+
+
+
+    def forward(self, x):
+        # model structure
+        pass
+
