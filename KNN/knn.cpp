cc3546b87db24f8d5fac758ffa47c9c6565d52d7
eric100789 <eric100147@gmail.com>
2023-03-26 20:27:40 +0800

    function added

---
+++ KNN/knn.cpp
@@ -0,0 +1,61 @@
+#include "knn.h"
+
+knn :: knn(int dimension, vector<vector<double>> train_data, vector<int> train_label, vector<vector<double>> test_data, vector<int> test_label)
+{
+    this->dimension = dimension;
+    this->train_data = train_data;
+    this->train_label = train_label;
+    this->test_data = test_data;
+    this->test_label = test_label;
+}
+
+void knn::normalize()
+{
+    int ROW = train_data.size();
+    int ROW_TEST = test_data.size();
+    for(int d=0 ; d<dimension ; d++)
+    {
+        double MAX = INT_MIN;
+        double MIN = INT_MAX;
+        for(int row=0; row<ROW ; row++)
+        {
+            if(train_data[row][d] > MAX) MAX = train_data[row][d];
+            if(train_data[row][d] < MIN) MIN = train_data[row][d];
+        }
+        train_max.push_back(MAX);
+        train_min.push_back(MIN);
+    }
+    
+    for(int d=0 ; d<dimension ; d++)
+    {
+        for(int row=0; row<ROW ; row++)
+        {
+            train_data[row][d] = (train_data[row][d] - train_min[d])/(train_max[d] - train_min[d]);
+        }
+        for(int row=0; row<ROW_TEST ; row++)
+        {
+            test_data[row][d] = (test_data[row][d] - train_min[d])/(train_max[d] - train_min[d]);
+        }
+    }
+}
+
+double knn::distance(vector<double>& tar1, vector<double>& tar2)
+{
+    double dsum = 0;
+    for(int i=0; i<dimension; i++)
+    {
+        dsum += pow((tar1[i] - tar2[i]),2);
+    }
+    return sqrt(dsum);
+}
+
+int knn::classify(vector<double>& target)
+{
+    typedef struct
+    {
+        double distance;
+        int label;
+    } data;
+    
+    
+}
--- KNN/knn.h
+++ KNN/knn.h
@@ -3,12 +3,37 @@
 
 #include<iostream>
 #include<math.h>
+#include <vector>
+#include<fstream>
 
 using namespace std;
 
 class knn
 {
 
+public:
+    knn(int dimension, 
+        vector<vector<double>> train_data, 
+        vector<int> train_label, 
+        vector<vector<double>> test_data,
+        vector<int> test_label);
+
+    void normalize();
+
+    double distance(vector<double>& tar1, vector<double>& tar2);
+
+    int classify(vector<double>& target);
+
+protected:
+    int dimension;
+    int k;
+    vector<vector<double>> train_data;
+    vector<int> train_label;
+    vector<double> train_max;
+    vector<double> train_min;
+    vector<vector<double>> test_data;
+    vector<int> test_label;
+    vector<int> ans_label;
 };
 
 class knn_diabetes : public knn
